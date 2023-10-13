76c51219d8a8f0ebdfac7843b369fa1a20ebf113
Sankrub <navathon.l@ku.th>
2023-10-13 23:10:14 +0700

    add unscribe

--- Backend/Controllers/subController.js
+++ Backend/Controllers/subController.js
@@ -13,14 +13,35 @@ exports.addSub  = async (req, res) => {
         const user = await user_model.findById(User_Id)
 
         if (user.Subscribe.includes(sportToSubscribe)) {
-            return res.status(400).json({ error: "You have been subscribed to this sport." });
+            return res.status(400).json({ error: `You have been subscribed to this ${sportToSubscribe}.` });
         }
         user.Subscribe.push(sportToSubscribe);
         await user.save();
-        res.json({ message: "Successfully subscribed to ${sportToSubscribe}." })
+        res.json({ message: `Successfully subscribed to ${sportToSubscribe}.`})
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Internal Server Error" });
     }
 }
+
+exports.unSub = async (req, res) => {
+    try {
+        const User_Id = req.params.id;
+        const sportToUnSubscribe = req.body.Sport;
+        const user = await user_model.findById(User_Id);
+
+        if (user.Subscribe.includes(sportToUnSubscribe)) {
+            user.Subscribe.splice(user.Subscribe.indexOf(sportToUnSubscribe), 1);
+            await user.save();
+            return res.json({ message: `Successfully Unsubscribed to ${sportToUnSubscribe}.` });
+        }
+        return res.status(400).json({ error: `You have not been subscribed to ${sportToUnSubscribe}.` });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+
 
