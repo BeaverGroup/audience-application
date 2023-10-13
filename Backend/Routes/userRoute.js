5314eb885f8cdba6db5bf02cbd1461618632c596
Sankrub <navathon.l@ku.th>
2023-10-13 23:10:55 +0700

    add route for unsribe

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -12,8 +12,10 @@ const {
 } = require("../Controllers/userController");
 
 const { addSub } = require("../Controllers/subController")
+const { unSub } = require("../Controllers/subController")
 const jsonParser = bodyParser.json();
-router.post("/subscribe/:id", jsonParser, addSub);
+router.post("/subscribe/:id", jsonParser, addSub); // add sport into subscribe (http://localhost:3002/user/subscribe/:id)
+router.delete("/unsubscribe/:id", jsonParser, unSub); // unsubcribe sport from subscribe (http://localhost:3002/user/unsubscribe/:id)
 require("dotenv").config();
 
 // ADD : check mail used before sending crateUser
