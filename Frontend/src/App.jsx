7c3998b4a799048476b90dea8a7e06c5fc108bc3
breezjirasak <jirasak.tu@ku.th>
2023-10-23 00:08:42 +0700

    add navbar component into the app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import HorizontalNav from "./components/horizontal_navbar/HorizontalNav";
+import VerticalNav from "./components/vertical_navbar/VerticalNav";
 
 function App() {
 
   return (
       <BrowserRouter>
       {/* navigateBar here */}
+      <HorizontalNav/>
+      <VerticalNav/>
         <Routes>
             <Route>{/* page */}</Route>
             <Route>{/* page */}</Route>
