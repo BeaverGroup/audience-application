6f0d89722d8f761e7fe86cd20c6b883835d73e0a
breezjirasak <jirasak.tu@ku.th>
2023-10-23 23:21:14 +0700

    implement navbar changed following screen size

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,21 +2,40 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import HorizontalNav from "./components/horizontal_navbar/HorizontalNav";
 import VerticalNav from "./components/vertical_navbar/VerticalNav";
+import { useState, useEffect } from 'react';
+import { widthToChangeNav, heightToChangeNav } from "./services/constants";
+
 
 function App() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
 
+  useEffect(() => {
+    const handleResizeWindow = () => {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    };
+    window.addEventListener("resize", handleResizeWindow);
+
+
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    }
+  }, []);
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       {/* navigateBar here */}
-      <HorizontalNav/>
-      <VerticalNav/>
-        <Routes>
-            <Route>{/* page */}</Route>
-            <Route>{/* page */}</Route>
-            <Route>{/* page */}</Route>
-        </Routes>
-        {/* footer here */}
-      </BrowserRouter>
+      <HorizontalNav />
+
+      {screenWidth < widthToChangeNav || screenHeight < heightToChangeNav ? "" : <VerticalNav />}
+      {/* <VerticalNav/> */}
+      <Routes>
+        <Route>{/* page */}</Route>
+        <Route>{/* page */}</Route>
+        <Route>{/* page */}</Route>
+      </Routes>
+      {/* footer here */}
+    </BrowserRouter>
   )
 }
 
