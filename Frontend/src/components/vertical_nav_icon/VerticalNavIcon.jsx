cbff971926511264995b39512b7a624da6a6360e
breezjirasak <jirasak.tu@ku.th>
2023-10-23 23:22:16 +0700

    import data from navbar contents

--- Frontend/src/components/vertical_nav_icon/VerticalNavIcon.jsx
+++ Frontend/src/components/vertical_nav_icon/VerticalNavIcon.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 import "./vertical_nav_icon.css";
 
 function VerticalNavIcon(props) {
-    const { navigate } = props;
+    const { navigate: content } = props;
     const [hovered, setHovered] = useState(false);
 
     useEffect(() => {
-        const element = document.getElementById(`${navigate.id}`);
+        const element = document.getElementById(`${content.id}`);
         function handleMouseOver() {
             setHovered(true);
         }
@@ -24,14 +24,14 @@ function VerticalNavIcon(props) {
             element.removeEventListener("mouseover", handleMouseOver);
             element.removeEventListener("mouseout", handleMouseOut);
         };
-    }, [navigate.id]);
+    }, [content.id]);
 
     return (
         <li>
             <div className="image-icon">
-                <Link to={navigate.to} id={navigate.id}>
-                    <img src={navigate.icon} alt="" />
-                    <span className={hovered ? "open" : ""}>{navigate.name}</span>
+                <Link to={content.to} id={content.id}>
+                    <img src={content.icon} alt="" />
+                    <span className={hovered ? "open" : ""}>{content.name}</span>
                 </Link>
             </div>
         </li>
--- Frontend/src/components/vertical_navbar/VerticalNav.jsx
+++ Frontend/src/components/vertical_navbar/VerticalNav.jsx
@@ -1,36 +1,6 @@
 import "./vertical_nav.css";
-import homeIcon from "../../icons/home.svg";
-import subIcon from "../../icons/subscribe.svg";
-import matchIcon from "../../icons/match.svg";
-import resultIcon from "../../icons/result.svg";
 import VerticalNavIcon from "../vertical_nav_icon/VerticalNavIcon";
-
-const navigates = [
-    {
-        id: "1",
-        name: "Home",
-        icon: homeIcon,
-        to: "/",
-    },
-    {
-        id: "2",
-        name: "Subscribe",
-        icon: subIcon,
-        to: "/subscribe",
-    },
-    {
-        id: "3",
-        name: "Match",
-        icon: matchIcon,
-        to: "/",
-    },
-    {
-        id: "4",
-        name: "Result",
-        icon: resultIcon,
-        to: "/",
-    },
-];
+import contents from "../../data/navbarContent";
 
 function VerticalNav() {
 
@@ -38,8 +8,8 @@ function VerticalNav() {
         <div>
             <nav className="vertical-nav">
                 <ul className="navigate">
-                    {navigates.map((navigate, index) => {
-                        return <VerticalNavIcon navigate={navigate} key={index} />;
+                    {contents.map((content, index) => {
+                        return <VerticalNavIcon navigate={content} key={index} />;
                     })}
                 </ul>
             </nav>
