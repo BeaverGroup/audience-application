7e343d0e8413da36ef9d0214a7fc5c564d89f863
natekrth <natekrth@gmail.com>
2023-10-25 00:31:44 +0700

    add mockupdata and icons

---
+++ Frontend/src/data/upcomingSubscribe.js
@@ -0,0 +1,24 @@
+const contents = [
+    {
+        id: "1",
+        name: "Home",
+        icon: homeIcon,
+    },
+    {
+        id: "2",
+        name: "Subscribe",
+        icon: subIcon,
+    },
+    {
+        id: "3",
+        name: "Match",
+        icon: matchIcon,
+    },
+    {
+        id: "4",
+        name: "Result",
+        icon: resultIcon,
+    },
+];
+
+export default contents;
