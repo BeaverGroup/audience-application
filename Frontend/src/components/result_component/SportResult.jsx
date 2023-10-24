3d63d14b3970f6501cda4c190e845d574174870e
panitnt <panittatanya@gmail.com>
2023-10-24 15:46:49 +0700

    Update result-component

--- Frontend/src/components/result_component/SportResult.jsx
+++ Frontend/src/components/result_component/SportResult.jsx
@@ -1,21 +1,45 @@
 /* eslint-disable no-unused-vars */
 import "./sport-result.css";
 import React from 'react'
+import { Icon } from '@iconify/react';
 
 const SportResult = () => {
     return (
         <div className='sport-result-card'>
-            <h5>Sport Result</h5>
-            <div className="sport-result-header">
-                <div>
-                    <div>
-                        Result
-                    </div>
-                    <div>
+            <h1>Sport Result</h1>
+            <table className="sport-result-table">
+                <tr>
+                    <th><h2>Sport</h2></th>
+                    <th><Icon className="country-flag" icon="twemoji:1st-place-medal" /></th>
+                    <th><Icon className="country-flag" icon="twemoji:2nd-place-medal" /></th>
+                    <th><Icon className="country-flag" icon="twemoji:3rd-place-medal" /></th>
+                </tr>
+                <tr>
+                    <td className="sport-result-table-name">
+                        <h2>Badminton</h2>
+                    </td>
+                    <td>
+                        <div>
+                            <Icon className="country-flag" icon="twemoji:flag-spain" />
+                            <p>Spain</p>
+                        </div>
+                    </td>
+                    <td>
+                        <div>
+                            <Icon className="country-flag" icon="twemoji:flag-france" />
+                            <p>France</p>
+                        </div>
+                    </td>
+                    <td>
+                        <div>
+                            <Icon className="country-flag" icon="twemoji:flag-china" />
+                            <p>China</p>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            <div>
 
-                    </div>
-                </div>
-                <hr />
             </div>
         </div>
     )
