05bd5f170db4b6ff1cbb6396775780623e108c55
natekrth <natekrth@gmail.com>
2023-10-31 22:05:40 +0700

    add upcomingmatchdetail component

---
+++ Frontend/src/components/upcoming_match_detail/UpcomingMatchDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpcomingMatchDetail = () => {
+  return (
+    <div className='upcoming-box-detail'>
+      Uasdjasdoiajsdioj
+    </div>
+  )
+}
+
+export default UpcomingMatchDetail
--- Frontend/src/pages/upcoming_match/UpcomingMatch.jsx
+++ Frontend/src/pages/upcoming_match/UpcomingMatch.jsx
@@ -1,22 +1,13 @@
 /* eslint-disable no-unused-vars */
-import AddSubscribeBox from "../../components/add_subscribe_box/AddSubscribeBox";
-import SportResult from "../../components/result_component/SportResult";
-import SubscribeBox from "../../components/subscribe_box/SubscribeBox";
-// import SubscribeSport from "../../components/subscribe_sport/SubscribeSport";
+import UpcomingMatchDetail from "../../components/upcoming_match_detail/UpcomingMatchDetail";
 import "./upcoming-match.css";
 import React, { useState, useEffect } from "react";
 
 const UpcomingMatch = () => {
   return (
-    <div className="upcoming-matches">
-      <div className="sport-bar">
-        <SubscribeBox />
-        <AddSubscribeBox />
-      </div>
-      <div className="sport-result-medal">
-        <SportResult />
-      </div>
-
+    <div className="upcoming-page">
+      test
+      <UpcomingMatchDetail/>
     </div>
   );
 };
