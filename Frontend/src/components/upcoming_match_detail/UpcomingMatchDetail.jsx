0a970bb8187963a54420e7372a94b1106aedda9f
natekrth <natekrth@gmail.com>
2023-10-31 22:41:40 +0700

    page structure

--- Frontend/src/components/upcoming_match_detail/UpcomingMatchDetail.jsx
+++ Frontend/src/components/upcoming_match_detail/UpcomingMatchDetail.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import "./upcoming_match_detail.css";
 
 const UpcomingMatchDetail = () => {
   return (
     <div className='upcoming-box-detail'>
-      Uasdjasdoiajsdioj
+      <h2>England VS Thailand</h2>
+      <div>
+        <p>Sportname: </p>
+        <p>Date: </p>
+        <p>Other Detail: </p>
+      </div>
+      <div>
+        Match Polls
+        <div>
+          <button>Thailand</button>
+          <button>England</button>
+        </div>
+      </div>
     </div>
   )
 }
