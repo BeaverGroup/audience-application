90a2188b09526da0224a3a02c2d20380b0c23713
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-11-01 02:11:20 +0700

    Implement search sport name

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -2,8 +2,24 @@ import UpcomingBox from '../../components/Upcoming/upcoming_box/UpcomingBox'
 import SportResult from '../../components/result_component/SportResult'
 import './home.css'
 import parisIcon from "../../icons/paris-icon.svg"
+import sports from "../../data/sports"
+import { useState } from 'react';
+import SportBox from '../../components/sport_box/SportBox'
+
+
+const getFiltered = (text, items) => {
+    if (!text) {
+        return items
+    }
+    return items.filter((sport) => sport.name.toLowerCase().includes(text.toLowerCase()))
+}
 
 const Home = () => {
+    const [text, setText] = useState("");
+
+    const filteredItems = getFiltered(text, sports)
+
+
     return (
         <div className='homepage'>
             <div className="section-1">
@@ -18,16 +34,22 @@ const Home = () => {
                 <div className="news-section">
                     <h2 className='news-title'>NEWS from your subscribe</h2>
                     <div className='upcoming-box'>
-                    <UpcomingBox/>
+                        <UpcomingBox />
                     </div>
                     <div className="result-box">
-                    <SportResult/>
+                        <SportResult />
                     </div>
-                    
-        
+
+
                 </div>
                 <div className="sports-section">
                     <h2 className='sports-title'>Sports</h2>
+                    <div className='search-sport'>
+                        <input placeholder='Search...' type="text" onChange={(e) => setText(e.target.value)} />
+                    </div>
+                    {filteredItems.map((item, index) => {
+                        return <SportBox sport={item} key={index}/>
+                    })}
                 </div>
             </div>
         </div>
