9c27fe13109e8357e746f36922d180b307bca89b
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-01 15:14:04 +0700

    verify cookie token when req vote and sub api

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -19,7 +19,6 @@ const {
 } = require("../Controllers/subController");
 
 const {
-  checkTokenGMiddle,
   verifyCookieToken,
 } = require("../Middlewares/authMiddle");
 const jsonParser = bodyParser.json();
@@ -37,7 +36,7 @@ router.get("/list", jsonParser, verifyCookieToken, allinfoUser); // register use
 router.post("/updateByID", jsonParser, verifyCookieToken, updateUserByID); // update user
 router.post("/updateByEmail", jsonParser, verifyCookieToken, updateUserByEmail); // update user
 router.delete("/removeByID", jsonParser, verifyCookieToken, removeUserByID); // register user (http://localhost:3002/user/infoByID)
-router.delete("/removeByEmail", jsonParser, removeUserByEmail); // register user (http://localhost:3002/user/infoByID)
+router.delete("/removeByEmail", jsonParser,verifyCookieToken ,removeUserByEmail); // register user (http://localhost:3002/user/infoByID)
 
 // vote
 router.delete("/unvote/:id", jsonParser, verifyCookieToken, removeVote);
