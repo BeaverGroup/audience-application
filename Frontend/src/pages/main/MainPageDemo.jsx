4832857bf3a2e353b11db31b7529fc23df4deeb3
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-07 18:18:19 +0700

    demo main page add new test api req

--- Frontend/src/pages/main/MainPageDemo.jsx
+++ Frontend/src/pages/main/MainPageDemo.jsx
@@ -6,14 +6,7 @@ import axios from "axios";
 function MainPage() {
   const { userState, setUserState } = useContext(UserStateContext);
   console.log("userState : ", userState);
-  if (!userState) {
-    return (
-      <div>
-        <h1>MainPage</h1>
-        <h3> Not found cookie token pls sign in </h3>
-      </div>
-    );
-  }
+
   const test = async () => {
     const port = import.meta.env.VITE_API_PORT;
     const host_ip = import.meta.env.VITE_API_HOST_IP;
@@ -26,6 +19,34 @@ function MainPage() {
       console.log(e);
     }
   };
+  const health = async () => {
+    const port = import.meta.env.VITE_API_PORT;
+    const host_ip = import.meta.env.VITE_API_HOST_IP;
+    try {
+      const all_user = await axios.get(`http://${host_ip}:${port}/health`, {
+        withCredentials: true,
+      });
+      console.log(all_user.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  if (!userState) {
+    return (
+      <div>
+        <h1>MainPage</h1>
+        <h3> Not found cookie token pls sign in </h3>
+        <button
+          style={{ width: "70%", height: "50px" }}
+          id="button_auth"
+          type="submit"
+          onClick={health}
+        >
+          test request api (non auth)
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div
