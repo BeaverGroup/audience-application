3ea74d71b31d8b8bcbcc79c35a5e181f29fbb49e
panitnt <panittatanya@gmail.com>
2023-11-11 22:49:39 +0700

    Add sample data

---
+++ Frontend/src/data/sampleData.js
@@ -0,0 +1,31 @@
+const sampleData = [
+  {
+    sport_name: "Football final match",
+    sport_id: "00001",
+    sport_type: "football",
+    round: "Final",
+    datetime: new Date("2023-11-08T12:00:00Z"),
+    medal: null,
+    participating_country: ["Spain", "England"],
+  },
+  {
+    sport_name: "Basketball final match",
+    sport_id: "00002",
+    sport_type: "basketball",
+    round: "Final",
+    datetime: new Date("2023-11-09T12:00:00Z"),
+    medal: null,
+    participating_country: ["Italy", "England"],
+  },
+  {
+    sport_name: "Badminton final match",
+    sport_id: "00003",
+    sport_type: "badminton",
+    round: "Final",
+    datetime: new Date("2023-11-09T12:00:00Z"),
+    medal: null,
+    participating_country: ["China", "Thailand"],
+  },
+];
+
+export default sampleData
