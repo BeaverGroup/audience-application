c8ff3847e1170914fdbe54de408fd72abf422223
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-13 23:42:46 +0700

    set rounter of reduceCountryCount

--- Backend/Routes/countryRoute.js
+++ Backend/Routes/countryRoute.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const bodyParser = require("body-parser");
-const { addCountryCount } = require("../Controllers/countryController");
+const { addCountryCount,reduceCountryCount } = require("../Controllers/countryController");
 const jsonParser = bodyParser.json();
 
 router.post("/add/:country", jsonParser, addCountryCount);
+// 
+router.post("/reduce/:country", jsonParser, reduceCountryCount);
+
 
 module.exports = router;
