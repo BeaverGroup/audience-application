3dc3e2cce383cdd827256469ca57b1e488ab601e
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-13 23:35:09 +0700

    function middleware of country about add country

---
+++ Backend/Controllers/countryController.js
@@ -0,0 +1,35 @@
+const { country_model } = require("../Models/countryModel.js");
+
+exports.addCountryCount = async (req, res) => {
+  try {
+    // Check if the country already exists
+    const countryParam = req.params.country;
+
+    const countryData = await country_model.findOne({ country: countryParam });
+
+    if (countryData) {
+      // If the country exists, increment the count
+      countryData.count += 1;
+      const newCountry = await countryData.save();
+      res.json({
+        message: `Successfully incremented ${countryParam}.`,
+        cID: newCountry._id,
+        country: newCountry.country,
+        count: newCountry.count,
+      });
+    } else {
+      // If the country does not exist, create a new one with count 1
+      const newCountry = new country_model({ country: countryParam, count: 1 });
+      await newCountry.save();
+      res.json({
+        message: `Successfully added ${countryParam} with count 1.`,
+        cID: newCountry._id,
+        country: newCountry.country,
+        count: newCountry.count,
+      });
+    }
+  } catch (err) {
+    console.error("Error in increaseCountryCount:", err);
+    throw err; // Rethrow the error to handle it in the calling function
+  }
+};
