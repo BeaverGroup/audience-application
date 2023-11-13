a45379f2adc962229fb13475f6e0e15b3f255dcb
natekrth <natekrth@gmail.com>
2023-11-14 01:37:05 +0700

    new data for subscribe result

---
+++ Frontend/src/data/subscribeUpcoming.js
@@ -0,0 +1,15 @@
+import homeIcon from "../icons/home.svg";
+import subIcon from "../icons/subscribe.svg";
+import matchIcon from "../icons/match.svg";
+import resultIcon from "../icons/result.svg";
+import { Icon } from '@iconify/react';
+
+const picture = {
+    Football: homeIcon,
+    Badminton: resultIcon,
+    Basketball: matchIcon,
+    Athletics: resultIcon,
+    Swimming: subIcon
+}
+
+export default picture
