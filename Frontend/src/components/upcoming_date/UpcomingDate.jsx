f933125ae93f43ccf5345843d0ab3253ad931aa3
panitnt <panittatanya@gmail.com>
2023-11-16 15:20:00 +0700

    Link data to show in upcoming dtae

--- Frontend/src/components/upcoming_date/UpcomingDate.jsx
+++ Frontend/src/components/upcoming_date/UpcomingDate.jsx
@@ -4,17 +4,36 @@ import UpcomingEachDay from './UpcomingEachDay'
 import sampleData from '../../data/sampleData'
 
 const UpcomingDate = () => {
+    const today = new Date();
+    const thisMonth = today.getMonth()+1;
+    const thisYear = today.getFullYear();
+    const thisDate = today.getDate();
+    // const dateAndTime = new Date(data,datetime)
+    const dateData = {}
+    for (let index = 1; index < 4; index++) {
+        // const startDateFormat = new Date(`${thisYear}-${thisMonth}-${index}T00:00:00.000Z`)
+        // const endDateFormat = new Date(`${thisYear}-${thisMonth}-${index}T23:59:59.999Z`)
+        let dateIn = `${index}`
+        if (index < 10){
+            dateIn = `0${index}`
+        }
+        const startDateFormat = new Date(`2024-08-${dateIn}T00:00:00.000Z`)
+        const endDateFormat = new Date(`2024-08-${dateIn}T23:59:59.999Z`)
+        const todayData = sampleData.filter(dt=> startDateFormat <= new Date(dt.datetime) && new Date(dt.datetime) <= endDateFormat)
+        dateData[`${index}`] = todayData
+    }
+    const dayList = Object.keys(dateData)
+    
     return (
         <div className='upcoming-date'>
             <h1>July</h1>
-            <UpcomingEachDay date='29 WED' data={sampleData.filter(data => (
-                new Date("2024-07-29T00:00:00.000Z") <= new Date(data.datetime) && 
-                new Date(data.datetime) <= new Date("2024-07-29T23:59:59.999Z")
-            ))}/>
-            <UpcomingEachDay date='30 THU' data={sampleData.filter(data => (
-                new Date("2024-07-30T00:00:00.000Z") <= new Date(data.datetime) &&
-                new Date(data.datetime) <= new Date("2024-07-30T23:59:59.999Z")
-            ))}/>
+            {dayList.map((day)=> {
+                if (!dateData[day].length){
+                    return null
+                }
+                const dateFormat = `${day}`
+                return <UpcomingEachDay date={dateFormat} data={dateData[day]} />
+            })}
         </div>
     )
 }
--- Frontend/src/components/upcoming_date/UpcomingEachDay.jsx
+++ Frontend/src/components/upcoming_date/UpcomingEachDay.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import './upcoming-each-day.css'
-import UpcomingList from './UpcomingList'
+import React from "react";
+import "./upcoming-each-day.css";
+import UpcomingList from "./UpcomingList";
 
 const UpcomingEachDay = (props) => {
-    const dataInDay = props.data
-    const newTime = new Date(props.data[0].datetime.slice(0, 19).concat("Z"))
+    const dataInDay = props.data;
     return (
-        <div className='upcoming-each-day'>
-            <div className='each-day-date'>
-                {props.date}
-            </div>
-            <div className='each-day-list'>
-                {dataInDay.map(day => <UpcomingList key={day.sport_id} link={"/upcoming/"+day.sport_id} name={day.sport_name} sport={day.sport_type} time={newTime.toLocaleTimeString()} data={day}/>)}
+        <div className="upcoming-each-day">
+            <div className="each-day-date">{props.date}</div>
+            <div className="each-day-list">
+                {dataInDay.map((day) => {
+                    const newTime = new Date(
+                        day.datetime.slice(0, 19).concat("Z")
+                    );
+
+                    return (
+                        <UpcomingList
+                            key={day.sport_id}
+                            link={"/upcoming/" + day.sport_id}
+                            name={day.sport_name}
+                            sport={day.sport_type}
+                            time={newTime.toLocaleTimeString()}
+                            data={day}
+                        />
+                    );
+                })}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default UpcomingEachDay
+export default UpcomingEachDay;
