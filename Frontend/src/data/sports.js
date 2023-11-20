9124452d938b5d02b6db6db062fb94954ef0c080
geeegrace <nichakorn.cha@ku.th>
2023-11-20 16:28:51 +0700

    Add sport image urls in sport boxes

--- Frontend/src/data/sports.js
+++ Frontend/src/data/sports.js
@@ -9,13 +9,13 @@ const sports = [
         "name": "Artistic Gymnastics",
         "icon": "mdi:gymnastics",
         "description": "Gymnastic discipline that emphasizes balance, strength, and creative routines on various apparatus.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://ichef.bbci.co.uk/news/976/cpsprodpb/035A/production/_131285800_gettyimages-1437774283.jpg.webp"
     },
     {
         "name": "Artistic Swimming",
         "icon": "icon-park-outline:swimming-ring",
         "description": "Synchronized swimming discipline where athletes perform synchronized routines in the water.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://swimswam.com/wp-content/uploads/2021/05/20210512_PMes_PM25981.jpg"
     },
     {
         "name": "Athletics",
@@ -27,29 +27,29 @@ const sports = [
         "name": "Badminton",
         "icon": "icon-park-outline:badminton",
         "description": "Racket sport played with a shuttlecock, emphasizing agility and precision.",
-        "image_url": "https://cdn.shopify.com/s/files/1/0020/9407/1890/files/2_480x480.jpg?v=1559302854"
+        "image_url": "https://ichef.bbci.co.uk/news/976/cpsprodpb/13F1F/production/_119659618_gettyimages-1331195453.jpg.webp"
     },
     {
         "name": "BMX Freestyle",
         "description": "Extreme sport involving performing tricks on a BMX bike.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://img.olympics.com/images/image/private/t_s_w2440/t_s_16_9_g_auto/f_auto/primary/tq6y84vllxljou98yvlt"
     },
     {
         "name": "BMX Racing",
         "description": "BMX bike racing on a dirt track with jumps and obstacles.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://medias.paris2024.org/uploads/2020/11/1331385293-min-scaled.jpeg?x-oss-process=image/resize,w_2560,h_1555,m_lfit/format,webp"
     },
     {
         "name": "Boxing",
         "icon": "ri:boxing-line",
         "description": "Combat sport involving punches and defensive techniques.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://www.gbboxing.org.uk/wp-content/uploads/2021/06/DSC_0814.jpg"
     },
     {
         "name": "Sailing",
         "icon": "game-icons:sport-medal",
         "description": "Competitive sailing events involving various classes of boats.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://library.sportingnews.com/styles/crop_style_16_9_desktop_webp/s3/2021-08/matt-wearn-gold-sailing_1f74r4n3mak4b1qpmikp475btb.png.webp"
     },
     {
         "name": "Skateboarding",
@@ -61,18 +61,18 @@ const sports = [
         "name": "Taekwondo",
         "icon": "game-icons:sport-medal",
         "description": "Korean martial art emphasizing high, fast kicks and jumping and spinning kicks.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://live-production.wcms.abc-cdn.net.au/9f7997b93fc0cfe2eeef964cd35be95d?impolicy=wcms_crop_resize&cropH=2707&cropW=4812&xPos=0&yPos=339&width=862&height=485"
     },
     {
         "name": "Tennis",
         "icon": "game-icons:sport-medal",
         "description": "Racket sport played individually or in pairs, involving a net and a ball.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://img.olympics.com/images/image/private/t_s_w2440/t_s_16_9_g_auto/f_auto/primary/rhbmgvuy8knmi7xfi0ce"
     },
     {
         "name": "Weightlifting",
         "description": "Sport involving the lifting of heavy weights in a specified manner.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://www.usatoday.com/gcdn/presto/2021/07/25/USAT/a5516acb-5283-443b-ab58-352fd1314412-GTY_1330515254.jpg?width=1320&height=944&fit=crop&format=pjpg&auto=webp"
     },
 ]
 
