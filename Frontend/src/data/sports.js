7453506d2394d7cfd326d439fa6a70c6aa8b232f
natekrth <natekrth@gmail.com>
2023-11-20 14:51:08 +0700

    add image SportBox

--- Frontend/src/data/sports.js
+++ Frontend/src/data/sports.js
@@ -2,17 +2,17 @@ const sports = [
     {
         "name": "Archery",
         "description": "Competitive sport involving shooting arrows at a target with a bow.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://olympic.ca/wp-content/uploads/2021/07/Team-Canada-Steaphanie-Barrett-Archery-Tokyo-2020-e1651260901654.jpg?quality=100"
     },
     {
         "name": "Athletics",
         "description": "Track and field events that encompass running, jumping, throwing, and more.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://img.olympics.com/images/image/private/t_s_w960/t_s_16_9_g_auto/f_auto/primary/hiuf5ahd3cbhr11q6m5m"
     },
     {
         "name": "Badminton",
         "description": "Racket sport played with a shuttlecock, emphasizing agility and precision.",
-        "image_url": "https://mir-s3-cdn-cf.behance.net/projects/404/63dcf7128656315.Y3JvcCwxOTk5LDE1NjQsMCw0.png"
+        "image_url": "https://cdn.shopify.com/s/files/1/0020/9407/1890/files/2_480x480.jpg?v=1559302854"
     },
     {
         "name": "BMX Freestyle",
