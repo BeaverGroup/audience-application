43eed34a5a49d007870cb989eb86d5dd0e0983fb
natekrth <natekrth@gmail.com>
2023-11-21 23:55:17 +0700

    add sport picture in data sportPicture.js

--- Frontend/src/data/sportPicture.js
+++ Frontend/src/data/sportPicture.js
@@ -5,6 +5,16 @@ const picture = {
     ATHLETICS: "https://img.olympics.com/images/image/private/t_s_w960/t_s_16_9_g_auto/f_auto/primary/hiuf5ahd3cbhr11q6m5m",
     SWIMMING: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH2m8ZTllbdET__tyEdrslOp5vLxzTGwCMiQ&usqp=CAU",
     ARCHERY: "https://olympic.ca/wp-content/uploads/2021/07/Team-Canada-Steaphanie-Barrett-Archery-Tokyo-2020-e1651260901654.jpg?quality=100",
-    "BMX RACING": "https://medias.paris2024.org/uploads/2020/11/1331385293-min-scaled.jpeg?x-oss-process=image/resize,w_2560,h_1555,m_lfit/format,jpeg"
+    "BMX RACING": "https://medias.paris2024.org/uploads/2020/11/1331385293-min-scaled.jpeg?x-oss-process=image/resize,w_2560,h_1555,m_lfit/format,jpeg",
+    SAILING: "https://library.sportingnews.com/styles/crop_style_16_9_desktop_webp/s3/2021-08/matt-wearn-gold-sailing_1f74r4n3mak4b1qpmikp475btb.png.webp",
+    TENNIS: "https://www.bhf.org.uk/-/media/images/information-support/heart-matters/heart-matters/summer-2018/activity/tennis_racket_balls_ss_0618_noexp_620x400.jpg?rev=c18a71fbd05e4a91b4bbc40af01aafaa&hash=B6D9F53AC70899560495FC5D3205784D",
+    "ARTISTIC GTMNASTICS": "https://ichef.bbci.co.uk/news/976/cpsprodpb/035A/production/_131285800_gettyimages-1437774283.jpg.webp",
+    "ARTISTIC SWIMMING": "https://swimswam.com/wp-content/uploads/2021/05/20210512_PMes_PM25981.jpg",
+    "BMX Freestyle": "https://img.olympics.com/images/image/private/t_s_w2440/t_s_16_9_g_auto/f_auto/primary/tq6y84vllxljou98yvlt",
+    "BMX Racing": "https://medias.paris2024.org/uploads/2020/11/1331385293-min-scaled.jpeg?x-oss-process=image/resize,w_2560,h_1555,m_lfit/format,webp",
+    BOXING: "https://www.gbboxing.org.uk/wp-content/uploads/2021/06/DSC_0814.jpg",
+    SKATEBOARDING: "https://api.time.com/wp-content/uploads/2021/08/us-skateboarding.jpg",
+    TAEKWONDO: "https://live-production.wcms.abc-cdn.net.au/9f7997b93fc0cfe2eeef964cd35be95d?impolicy=wcms_crop_resize&cropH=2707&cropW=4812&xPos=0&yPos=339&width=862&height=485",
+    WEIGHTLIFTING: "https://www.usatoday.com/gcdn/presto/2021/07/25/USAT/a5516acb-5283-443b-ab58-352fd1314412-GTY_1330515254.jpg?width=1320&height=944&fit=crop&format=pjpg&auto=webp"
 }
 export default picture
