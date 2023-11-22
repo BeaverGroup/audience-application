15198ea339aa3842dc68c0b922f721907ba0c5ec
Sankrub <navathon.l@ku.th>
2023-11-23 01:06:25 +0700

    update to enable run test

--- Backend/server.js
+++ Backend/server.js
@@ -5,27 +5,24 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config(); // Load environment variables from .env file
 
 const app = express();
-app.use(morgan("dev")); // console log when server using any api
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_IP })); // Enable CORS for all routes / check policies when requesting routes
-app.use(express.json()); // change json to javascript
+app.use(morgan("dev"));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_IP }));
+app.use(express.json());
 app.use(cookieParser());
 
+// Database connection and route requires remain the same
 const connectDB = require("./Config/db");
 connectDB();
 
-const healthController = require("./Controllers/healthController"); // adjust the path as necessary
+const healthController = require("./Controllers/healthController");
 const userRouter = require("./Routes/userRoute");
 const authRouter = require("./Routes/authRoute");
 const countryRouter = require("./Routes/countryRoute");
 
-app.use("/user", userRouter); // use and set prefix path of Insurance
+app.use("/user", userRouter);
 app.use("/auth", authRouter);
 app.get("/health", healthController.healthCheck);
-app.use("/country",countryRouter)
+app.use("/country", countryRouter);
 
 
-const PORT = process.env.SERVER_PORT || 4000; // Use the port from the environment variables or default to 4000
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT} V3`);
-});
+module.exports = app; // Export the app
